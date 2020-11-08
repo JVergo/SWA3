596d0995c2650bcb5f67e51780f679ccc0355551
Jvergo <Jvergoa@gmail.com>
2020-11-08 15:22:31 +0100

    Fixed class name error

--- Flux/src/pages/Home.js
+++ Flux/src/pages/Home.js
@@ -3,8 +3,8 @@ import React from "react";
 function Home() {
     return (
         <div className="jumbotron">
-            <h1 class="display-4">Welcome to the WeatherApp</h1>
-            <p class="lead">This is a Home page of WeatherApp.</p>
+            <h1 className="display-4">Welcome to the WeatherApp</h1>
+            <p className="lead">This is a Home page of WeatherApp.</p>
         </div>
     );
 }
